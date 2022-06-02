e7b153801612b7bb43bf0af9cc0dba97f483eba4
infosecmentor <muhambiphares@gmail.com>
2022-06-02 11:51:22 +0300

    commit tree 8b83b70f17d0b6ae677420a9bdad66a84447291e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+const names = ["James", "Zzerro", "Ssaru"];
+event = "surprise";
+
+
+function writeCards(names, event) {
+    let thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(
+            `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        );
+    }
+    return thankYouCards;
+
+}
+
+
+
+function countDown(startInt) {
+    while (startInt > 0) {
+        console.log(startInt);
+        startInt -= 1;
+    }
+    console.log(startInt);
+}
